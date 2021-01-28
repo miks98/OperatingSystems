c97d5c49f8fb6a44f469e8047923d508b3445dd1
M Iftikhar Uddin Khan Sami <hits89411a@gmail.com>
2021-01-29 00:31:10 +0500

    task3

--- Final Exam/OsLab/src/task3.java
+++ Final Exam/OsLab/src/task3.java
@@ -1,5 +1,14 @@
 //Java Program for Bankers Algorithm 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author MIKS
+ */
 
 class task3 {
     int n = 3;
